df8638cd5c85ea344cd80239115359e860919a7c
Marco Emrich <marco.emrich@codecentric.de>
2020-10-05 20:07:26 +0200

    WIP

--- Player.js
+++ Player.js
@@ -9,7 +9,20 @@ class Player {
     var game = new GameState(gameState);
     console.log("Hello");
     console.log(game);
-    bet(5);
+
+    const raiseLimit = 30;
+    const callLimit = 50;
+
+    if (game.toRaise() < raiseLimit) {
+      bet(game.toRaise());
+    } else {
+      if (game.toCall() < callLimit) {
+        bet(game.toCall());
+      } else {
+        bet(0);
+      }
+    }
+
   }
 
   static showdown(gameState) {
